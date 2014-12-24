7d493a94909cb62725a8ca2f7d96852f2454d4fb
Robert-Jan De Pauw <rjdepauw@gmail.com>
2014-12-24 10:49:37 +0100

    added angular bootstrap ui and bootstrap css dependencies

--- layout-edit/app/js/app.js
+++ layout-edit/app/js/app.js
@@ -2,33 +2,5 @@
 
 // Declare app level module which depends on views, and components
 var simpleDoc = angular.module('simpleDoc', [
-	'simpleDocControllers'	
+	'simpleDocControllers', 'ui.bootstrap'
 ]);
-// 'use strict';
-//
-// /* App Module */
-//
-// var phonecatApp = angular.module('phonecatApp', [
-//   'ngRoute',
-//   'phonecatAnimations',
-//
-//   'phonecatControllers',
-//   'phonecatFilters',
-//   'phonecatServices'
-// ]);
-//
-// phonecatApp.config(['$routeProvider',
-//   function($routeProvider) {
-//     $routeProvider.
-//       when('/phones', {
-//         templateUrl: 'partials/phone-list.html',
-//         controller: 'PhoneListCtrl'
-//       }).
-//       when('/phones/:phoneId', {
-//         templateUrl: 'partials/phone-detail.html',
-//         controller: 'PhoneDetailCtrl'
-//       }).
-//       otherwise({
-//         redirectTo: '/phones'
-//       });
-//   }]);
